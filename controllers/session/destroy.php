1df957e00e073f2b61e2c9dbe0bd9aa09096e378
Oubaidasl <oassladday@gmail.com>
2025-11-19 01:58:48 +0100

    Log in/Log out process is officially completed

--- controllers/session/destroy.php
+++ controllers/session/destroy.php
@@ -1,10 +1,34 @@
-<?php 
+<?php
+// Start the session if not already active
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
 
+// 1) Clear server-side session data
 $_SESSION = [];
+
+// 2) Delete the session cookie in the browser
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    // Use session_name() to get the actual cookie name (usually PHPSESSID)
+    setcookie(
+        session_name(),
+        '',
+        [
+            'expires' => time() - 42000,
+            'path' => $params['path'],
+            'domain' => $params['domain'],
+            'secure' => !empty($params['secure']),
+            'httponly' => true,
+            // If you set SameSite earlier, also include it here to match
+            'samesite' => $params['samesite'] ?? 'Lax',
+        ]
+    );
+}
+
+// 3) Destroy the session on the server
 session_destroy();
 
-$session = session_get_cookie_params();
-setcookie('PHPSSID', '', time() - 3600, $session['path'], $session['domain'], $session['secure'], $session['httponly']);
-
-header('location: /');
-exit();
+// 4) Redirect away from authenticated pages
+header('Location: /login');
+exit;
